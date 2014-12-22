63a8a7ee176c25f2f240a98f2743b0973b003b2a
Your Name <you@example.com>
2014-12-22 23:05:49 +0100

    refactor: information hiding with closure

--- web/js/duedate.js
+++ web/js/duedate.js
@@ -1,6 +1,8 @@
-var dueDateCalculator = function(startDate, hours) {
-  var diffMillis = hours * 60 * 60 * 1000,
-      startMillis = startDate.getTime();
+var dueDateCalculator = function() {
   
-  return new Date(startMillis + diffMillis);
-};
+  return function(startDate, hours) {    
+    var diffMillis = hours * 60 * 60 * 1000,
+        startMillis = startDate.getTime();  
+    return new Date(startMillis + diffMillis);
+  };
+}();
