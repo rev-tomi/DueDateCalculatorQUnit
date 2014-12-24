41acca3796197a11fffb293aa83bffc8baa5c68d
Your Name <you@example.com>
2014-12-24 18:39:06 +0100

    refactoring: using closure to protect internals of startOfNextPeriod

--- web/js/duedate.js
+++ web/js/duedate.js
@@ -17,29 +17,32 @@ dueDateCalculator.workTimeSchedule = {
     return 0 < day && day < 6 && 9 <= hour && hour < 17;
   },
   
-  'startOfNextPeriod' : function(time) {
-    var milliseconds = time.getTime(),
-        result = new Date(milliseconds),
-        hourInMillisec = 60 * 60 * 1000,
+  'startOfNextPeriod' : function() {
+    var hourInMillisec = 60 * 60 * 1000,
         dayInMillisec = 24 * hourInMillisec,
-        difference = 0,
         millisAsDateTrimmedToHours = function(millis) {
           var untrimmed = new Date(millis);
           return new Date(untrimmed.getFullYear(), untrimmed.getMonth(), untrimmed.getDate(), untrimmed.getHours());
         };
     
-    if (time.getDay() == 6) {
-      console.log('adding 2 days');
-      difference += 2 * dayInMillisec;
-    } else if (time.getDay() == 0) {
-      console.log('adding 1 day');
-      difference += dayInMillisec;
-    }
+    return function(time) {
+      var milliseconds = time.getTime(),
+          result = new Date(milliseconds),
+          difference = 0;
+     
+      if (time.getDay() == 6) {
+        console.log('adding 2 days');
+        difference += 2 * dayInMillisec;
+      } else if (time.getDay() == 0) {
+        console.log('adding 1 day');
+        difference += dayInMillisec;
+      }
     
-    difference += (9 - time.getHours()) * hourInMillisec;
+      difference += (9 - time.getHours()) * hourInMillisec;
     
-    console.log('original milliseconds: ' + milliseconds);
-    console.log('difference: ' + difference);
-    return millisAsDateTrimmedToHours(milliseconds + difference);
-  },
+      console.log('original milliseconds: ' + milliseconds);
+      console.log('difference: ' + difference);
+      return millisAsDateTrimmedToHours(milliseconds + difference);
+    };
+  }(),
 };
