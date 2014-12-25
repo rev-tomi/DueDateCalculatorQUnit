ec97ebec3132bcb6bdb00da0bc1787aac40576db
Your Name <you@example.com>
2014-12-25 15:10:15 +0100

    minor cleanup

--- web/js/duedate.js
+++ web/js/duedate.js
@@ -2,9 +2,9 @@ var dueDateCalculator = function() {
   
   var start = 9, end = 17;
   
-  return function(startDate, hours) {   
+  return function(startDate, hours) {
     var diffMillis = hours * 60 * 60 * 1000,
-        startMillis = startDate.getTime();  
+        startMillis = startDate.getTime();
     return new Date(startMillis + diffMillis);
   };
 }();
--- web/js/test.js
+++ web/js/test.js
@@ -1,14 +1,15 @@
 QUnit.test("dueDate", function(assert) {
   
-  var assertDueDate = function(start, hours, end, msg) {
-    var dueDate = dueDateCalculator(start, hours);
-    assert.equal(dueDate.getTime(), end.getTime(), msg);
+  var assertDueDate = function(start, hours, end, msg, verbose) {
+    var dueDate = dueDateCalculator(start, hours),
+        message = verbose ? msg + " " + start.toString() + " vs " + end.toString() : msg;
+    assert.equal(dueDate.getTime(), end.getTime(), message);
   },
   now = new Date();
   
   assertDueDate(now, 0, now, "Zero hours is identity");
   assertDueDate(new Date(2014, 12, 22, 9), 2, new Date(2014, 12, 22, 11), "Adding some time within the same day");
-  //assertDueDate(new Date(2014, 11, 22, 16, 0, 0, 0), 2, new Date(2014, 11, 23, 10), "Adding some time that ends the next day");
+  assertDueDate(new Date(2014, 11, 22, 15, 0, 0, 0), 2, new Date(2014, 11, 23, 9), "Adding some time that ends the next day", true);
 });
 
 QUnit.test("workTimeSchedule", function(assert) {
